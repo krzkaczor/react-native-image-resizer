2036ab10430796339ea770fc912d044bbfdaf174
Tim Suchanek <tim.suchanek@gmail.com>
2016-08-04 14:45:05 +0200

    fix exif rotation

--- android/src/main/java/fr/bamlab/rnimageresizer/ImageResizer.java
+++ android/src/main/java/fr/bamlab/rnimageresizer/ImageResizer.java
@@ -2,21 +2,17 @@ package fr.bamlab.rnimageresizer;
 
 import android.content.Context;
 import android.content.ContentResolver;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapRegionDecoder;
 import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.net.Uri;
+import android.media.ExifInterface;
+import android.database.Cursor;
 import android.provider.MediaStore;
-import android.util.Pair;
 
-import java.io.Closeable;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.IOException;
 import java.util.Date;
@@ -26,35 +22,129 @@ import java.util.Date;
  */
 class ImageResizer {
 
-    private static Bitmap resizeImage(Bitmap image, int maxWidth, int maxHeight, Context context) {
-        Bitmap newImage = null;
+    private static Bitmap rotateExif(String src, Bitmap bitmap) {
+        try {
+            ExifInterface ei = new ExifInterface(src);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
 
-        if (image == null) {
-            return null; // Can't load the image from the given path.
+            if (orientation == 1) {
+                return bitmap;
+            }
+
+            Matrix matrix = new Matrix();
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                    matrix.setScale(-1, 1);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    matrix.setRotate(180);
+                    break;
+                case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                    matrix.setRotate(180);
+                    matrix.postScale(-1, 1);
+                    break;
+                case ExifInterface.ORIENTATION_TRANSPOSE:
+                    matrix.setRotate(90);
+                    matrix.postScale(-1, 1);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    matrix.setRotate(90);
+                    break;
+                case ExifInterface.ORIENTATION_TRANSVERSE:
+                    matrix.setRotate(-90);
+                    matrix.postScale(-1, 1);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    matrix.setRotate(-90);
+                    break;
+                default:
+                    return bitmap;
+            }
+
+            try {
+                Bitmap oriented = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                bitmap.recycle();
+                return oriented;
+            } catch (OutOfMemoryError e) {
+                e.printStackTrace();
+                return bitmap;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        if (maxHeight > 0 && maxWidth > 0) {
-            float width = image.getWidth();
-            float height = image.getHeight();
-
-            float ratio = Math.min((float)maxWidth / width, (float)maxHeight / height);
-
-            int finalWidth = (int) (width * ratio);
-            int finalHeight = (int) (height * ratio);
-            newImage = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
-        }
-
-        return newImage;
+        return bitmap;
     }
 
-    public static Bitmap rotateImage(Bitmap source, float angle)
-    {
-        Bitmap retVal;
+    private static String getRealPathFromURI(Uri contentURI, Context context) {
+      String result;
+      Cursor cursor = context.getContentResolver().query(contentURI, null, null, null, null);
+      if (cursor == null) { // Source is Dropbox or other similar local file path
+          result = contentURI.getPath();
+      } else { 
+          cursor.moveToFirst(); 
+          int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA); 
+          result = cursor.getString(idx);
+          cursor.close();
+      }
+      return result;
+    }
 
-        Matrix matrix = new Matrix();
-        matrix.postRotate(angle);
-        retVal = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
-        return retVal;
+    private static Bitmap resizeImage(String imagePath, int maxWidth, int maxHeight, Context context) {
+        try {
+            Bitmap image;
+            if (!imagePath.startsWith("content://") && !imagePath.startsWith("file://")) {
+                image = BitmapFactory.decodeFile(imagePath);
+            } else {
+                ContentResolver cr = context.getContentResolver();
+                Uri url = Uri.parse(imagePath);
+                InputStream input = cr.openInputStream(url);
+                image = BitmapFactory.decodeStream(input);
+                image = rotateExif(getRealPathFromURI(url, context), image);
+                input.close();
+            }
+
+            if (image == null) {
+                return null; // Can't load the image from the given path.
+            }
+
+            if (maxHeight > 0 && maxWidth > 0) {
+                float width = image.getWidth();
+                float height = image.getHeight();
+
+                float ratio = Math.min((float)maxWidth / width, (float)maxHeight / height);
+
+                int finalWidth = (int) (width * ratio);
+                int finalHeight = (int) (height * ratio);
+                image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+            }
+
+            return image;
+        }catch (IOException ex) {
+             // No memory available for resizing.
+        }
+
+        return null;
+    }
+
+    public static Bitmap rotateImage(Bitmap b, float degrees)
+    {
+        if (degrees != 0 && b != null) {
+            Matrix m = new Matrix();
+            m.setRotate(degrees,
+                    (float) b.getWidth() / 2, (float) b.getHeight() / 2);
+            try {
+                Bitmap b2 = Bitmap.createBitmap(
+                        b, 0, 0, b.getWidth(), b.getHeight(), m, true);
+                if (b != b2) {
+                    b.recycle();
+                    b = b2;
+                }
+            } catch (OutOfMemoryError ex) {
+                // No memory available for rotating. Return the original bitmap.
+            }
+        }
+        return b;
     }
 
     private static String saveImage(Bitmap bitmap, File saveDirectory, String fileName,
@@ -84,119 +174,18 @@ class ImageResizer {
         return newFile.getAbsolutePath();
     }
 
-
-    /**
-     * Get {@link File} object for the given Android URI.<br>
-     * Use content resolver to get real path if direct path doesn't return valid file.
-     */
-    private static File getFileFromUri(Context context, Uri uri) {
-
-        // first try by direct path
-        File file = new File(uri.getPath());
-        if (file.exists()) {
-            return file;
-        }
-
-        // try reading real path from content resolver (gallery images)
-        Cursor cursor = null;
-        try {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            cursor = context.getContentResolver().query(uri, proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            String realPath = cursor.getString(column_index);
-            file = new File(realPath);
-        } catch (Exception ignored) {
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-
-        return file;
-    }
-
-
-    /**
-     * Get orientation by reading Image metadata
-     */
-    public static int getOrientation(Context context, Uri uri) {
-        try {
-            File file = getFileFromUri(context, uri);
-            if (file.exists()) {
-                ExifInterface ei = new ExifInterface(file.getAbsolutePath());
-                return getOrientation(ei);
-            }
-        } catch (Exception ignored) { }
-
-        return 0;
-    }
-
-    /**
-     * Convert metadata to degrees
-     */
-    public static int getOrientation(ExifInterface exif) {
-        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-        switch (orientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                return 90;
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                return 180;
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                return 270;
-            default:
-                return 0;
-        }
-    }
-
     public static String createResizedImage(Context context, String imagePath, int newWidth,
                                             int newHeight, Bitmap.CompressFormat compressFormat,
                                             int quality, int rotation, String outputPath) throws IOException  {
 
-
-
-        Bitmap sourceImage;
-        if (!imagePath.startsWith("content://") && !imagePath.startsWith("file://")) {
-            sourceImage = BitmapFactory.decodeFile(imagePath);
-        } else {
-            ContentResolver cr = context.getContentResolver();
-            Uri url = Uri.parse(imagePath);
-            InputStream input = cr.openInputStream(url);
-            sourceImage = BitmapFactory.decodeStream(input);
-            input.close();
-        }
-
-        // Start transformations:
-
-        // Scale it first so there are fewer pixels to transform in the rotation
-        Bitmap scaledImage = ImageResizer.resizeImage(sourceImage, newWidth, newHeight, context);
-        if (sourceImage != scaledImage) {
-            sourceImage.recycle();
-        }
-
-        // Rotate if neccesary
-        Bitmap rotatedImage = scaledImage;
-        if (rotation > 0) {
-            int orientation = getOrientation(context, Uri.parse(imagePath));
-            rotation = orientation + rotation;
-            rotatedImage = ImageResizer.rotateImage(scaledImage, rotation);
-        }
-
-        if (scaledImage != rotatedImage) {
-            scaledImage.recycle();
-        }
+        Bitmap resizedImage = ImageResizer.rotateImage(ImageResizer.resizeImage(imagePath, newWidth, newHeight, context), rotation);
 
         File path = context.getCacheDir();
-        if (outputPath != null) {
-          path = new File(outputPath);
-        }
+        //if (outputPath != null || !outputPath.isEmpty()) {
+          //path = new File(outputPath);
+        //}
 
-        String resizedImagePath = ImageResizer.saveImage(rotatedImage, path,
+        return ImageResizer.saveImage(resizedImage, path,
                 Long.toString(new Date().getTime()), compressFormat, quality);
-
-        // Clean up remaining image
-        rotatedImage.recycle();
-
-        return resizedImagePath;
     }
 }
